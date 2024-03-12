04a7d26a43e1acfd1d016a35c8b72a975ea2c2fb
ZouJoelin <572060671@qq.com>
2024-03-12 15:12:13 +0800

    adjust price

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from utils import *
 # initialize Flask.app & session & sqlite
 ###############################################
 
-PRICE_PER_PAGE_ONE = 0.12
+PRICE_PER_PAGE_ONE = 0.11
 PRICE_PER_PAGE_TWO = 0.10
 PRINTER_LOCATION = "东校慎思园6号文化室"
 UPLOAD_FOLDER = os.getcwd() + "/files_temp/"
--- bug_to_fixed/bugs.md
+++ bug_to_fixed/bugs.md
@@ -1,4 +1,4 @@
 
 
-* lpr -o fit-to-page -o media=A4 -o sides=one-sided -# 1 /home/server/Capybara/files_temp/2_准假单.pdf
+* lpr -o fit-to-page -o media=A4 -o sides=one-sided -# 1 /home/server/Capybara/bug_to_fixed/2_准假单.pdf
 
