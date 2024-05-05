5567286017110d7ff5dcbd4268279becd5b74465
ZouJoelin <572060671@qq.com>
2024-05-05 18:31:54 +0800

    ready to go online: V1

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -6,7 +6,7 @@
 
 
 # basic
-bind = '127.0.0.1:8080'                             #绑定监听ip和端口号
+bind = '127.0.0.1:8000'                             #绑定监听ip和端口号
 workers = 1 #multiprocessing.cpu_count() * 2 + 1       #同时执行的进程数，推荐为当前CPU个数*2+1
 worker_class = 'gevent'                               #sync, gevent,meinheld   #工作模式选择，默认为sync，这里设定为gevent异步
 worker_connections = 1000
